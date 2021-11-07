1dc25567c3f10523a373a116104f2e5783563375
JG <johngillard15@gmail.com>
2021-11-06 20:04:25 -0400

    added Customer PutMapping

--- src/main/java/com/careerdevs/Expressian/customer/CustomerController.java
+++ src/main/java/com/careerdevs/Expressian/customer/CustomerController.java
@@ -1,7 +1,9 @@
 package com.careerdevs.Expressian.customer;
 
+import com.careerdevs.Expressian.vehicle.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -13,8 +15,7 @@ public class CustomerController {
     @Autowired private CustomerRepository repository;
 
     @GetMapping
-    public @ResponseBody
-    List<Customer> getRepository() {
+    public @ResponseBody List<Customer> getRepository() {
         return repository.findAll();
     }
 
@@ -26,5 +27,15 @@ public class CustomerController {
     @PostMapping
     public @ResponseBody Customer createCustomer(@RequestBody Customer newCustomer){
         return repository.save(newCustomer);
+    }
+
+    @PutMapping("/{id}")
+    public @ResponseBody Customer updateCustomer(@PathVariable Long id, @RequestBody Customer updates){
+        Customer customer = repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        if(updates.getFirstName() != null) customer.setFirstName(updates.getFirstName());
+        if(updates.getLastName() != null) customer.setLastName(updates.getLastName());
+
+        return repository.save(customer);
     }
 }
