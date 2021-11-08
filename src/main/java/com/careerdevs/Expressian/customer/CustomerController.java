d37a93592ef1bf3f4cb9b119d4a47de0dd33d266
JG <johngillard15@gmail.com>
2021-11-08 18:39:28 -0500

    removed unused import

--- src/main/java/com/careerdevs/Expressian/customer/CustomerController.java
+++ src/main/java/com/careerdevs/Expressian/customer/CustomerController.java
@@ -1,6 +1,5 @@
 package com.careerdevs.Expressian.customer;
 
-import com.careerdevs.Expressian.vehicle.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
