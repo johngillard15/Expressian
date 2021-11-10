b89b804c6536f75a869d7c7bb12d98fa8d1e0f57
JG <johngillard15@gmail.com>
2021-11-09 20:52:25 -0500

    added Rental entity stuff

---
+++ src/main/java/com/careerdevs/Expressian/rental/RentalRepository.java
@@ -0,0 +1,5 @@
+package com.careerdevs.Expressian.rental;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RentalRepository extends JpaRepository<Rental, Long> {}
